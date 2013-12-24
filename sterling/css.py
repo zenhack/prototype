c7d607c4a54816d5a2fc1cd37a040db954ff1024
Ian Denhardt <ian@zenhack.net>
2013-12-23 19:00:12 -0500

    Started working on CSS matching library.
    We're using tinycss for parsing, but it doesn't do much more than
    tokenize the selectors, so most of this commit is parsing selectors.
    
    I've also commented out a few of the rules in the test css, since the
    parser currently can't handle them.

--- sterling/css.py
+++ sterling/css.py
@@ -22,226 +22,90 @@ import os
 import sys
 import logging
 
-def block(string, f1, f2, s):
-    """Return the start and end char positions of a block."""
-    i = string.find(f1, s)
-    j = string.find(f2, i)
-    return (i, j)
+import tinycss
 
-def parse(string, sep='=', eol='\n', t=str, start=0):
-    """Parse a block and return a dictionary"""
-    result = {}
-    while True:
-        (i, j) = block(string, sep, eol, start)
-        if i < 0 or j < 0 or j < i:
-            break
-        name = string[start:i].strip()
-        result[name] = t(string[i+1:j].strip())
-        start = j+1
-    return result
-
-def remove(string, left='"""', right='"""'):
-    """Remove a block of text"""
-    start = 0
-    while True:
-        (i, j) = block(string, left, right, start)
-        if i < 0 or j < 0 or j < i:
-            break
-        string = string[:i] + string[j+2:]
-        start = i
-    return string
-
-
-class StyleController(dict):
-    """Control the default values, give access to a simple
-    dictionary of types with sub dictionaries of properties"""
-    def __init__(self):
-        dict.__init__(self)
-        self['*'] = {}
-        self.filters = {}
-        self.lames = {}
-
-    def add_filters(self, filters):
-        """Add filters to apply to values when loaded."""
-        self.filters.update(filters)
-
-    def add_globals(self, namespace):
-        """Add names to be available in the css for all names."""
-        pass
-
-    def add_locals(self, namespaces):
-        """Add a dictionary of namespaces to be used on those names only."""
-        self.lames.update(namespaces)
-
-    def add_defaults(self, defaults):
-        """Add multiple defaults for different kinds of objects, '*' for all"""
-        for (otype, default) in defaults.iteritems():
-            self.set_default(otype, default)
-
-    def set_default(self, otype, *args, **kwargs):
-        """
-        Set multiple defaults for the given type:
-
-        defaults.set(type_id, name, value)
-        defaults.set(type_id, { 'name': value })
-        defaults.set(type_id, name=value)
-
-        """
-        target = self.setdefault(otype, {})
-        if args:
-            if isinstance(args[0], dict):
-                target.update(args[0])
-            else:
-                target[args[0]] = args[1]
-        if kwargs:
-            target.update(kwargs)
-
-    def get_default(self, otype, name=None):
-        """Return a dictionary of available defaults"""
-        if name:
-            return self._get_single_default(otype, name)
-        result = self['*'].copy()
-        if otype != None and self.has_key(otype):
-            result.update(self[otype])
-        return result
-
-    def _get_single_default(self, otype, name):
-        """Get the single default value for this type/name"""
-        if otype != None and self.has_key(otype):
-            if self[otype].has_key(name):
-                return self[otype][name]
-        return self['*'].get(name, None)
-
-    def has_default(self, otype, name):
-        """Returns true if there is a default value at all"""
-        if otype != None and self.has_key(otype):
-            if self[otype].has_key(name):
-                return True
-        return self['*'].has_key(name)
-
-    def get_value(self, name, value):
-        """Process the value from css and get the real value"""
-        if callable(value):
-            return value()
-        elif name in self.filters.keys():
-            # This value came from the css sheet and is handled.
-            return self.filters[name](value)
-        elif isinstance(value, basestring):
-            # This value came from the css sheet and is code.
-            # We could run code here, these are literals and we can handle them as choices in a dictionary or as some other data.
-            raise NotImplementedError("Literals not implimented");
-        else:
-            logging.error("Unknown type: %s" % type(value))
-
-
-class StyleSheet(object):
+class Stylesheet(object):
     """A single style sheet objct"""
-    def __init__(self, content=None):
-        self.styles = {}
-        if content:
-            content = remove(content, '/*', '*/')
-            content = remove(content, '//', '\n')
-            self.load(content)
+    def __init__(self, content):
+        self.rules = []
+        parser = tinycss.make_parser()
+        ast = parser.parse_stylesheet(content)
+        for rule in ast.rules:
+            selector = Selector(rule.selector)
+            self.rules.append((selector,rule))
 
-    def load(self, content):
-        """Parse some basic contents"""
-        s = parse(content, '{', '}')
-        for key in s.keys():
-            value = parse(s.pop(key), ':', ';', str)
-            for kins in key.split(','):
-                s[kins.strip()] = value
-        r = {}
-        for key in s.keys():
-            (name, state) = ':' in key and key.split(':') or (key, '')
-            if not r.has_key(name):
-                r[name] = {}
-            if r[name].has_key(state):
-                r[name][state].update(s[key])
+    def matches(self, **kwargs):
+        results = []
+
+        for selector, rule in self.rules:
+            if selector.match(**kwargs):
+                results.append((selector, rule))
+
+        return (results,[],[])
+
+class ParseError(Exception): pass
+
+class Selector(object):
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.name = None
+        self.id = None
+        self.cls = []
+        self._parse()
+
+    def _parse(self):
+        return self._parse_simple(self.tokens, 0)
+
+    def _parse_simple(self, tokens, idx):
+        if tokens[idx].type == 'IDENT':
+            self.name = tokens[idx].value
+            return self._parse_simple_suffix(tokens, idx + 1)
+        else:
+            new_idx = self._parse_simple_suffix(tokens, idx)
+            if new_idx <= idx:
+                raise ParseError("Empty simple selector")
             else:
-               r[name][state] = s[key]
-        self.styles = [ Style(name, r[name]) for name in r.keys() ]
+                return new_idx
 
-    def get_style(self, tid, oid, classes):
-        """Return a style controll for the given names"""
-        result = Styles()
-        for style in self.styles:
-            if style.style_match(tid, oid, classes):
-                result.append(style)
-                style.used = True
-        return result
+    def _parse_simple_suffix(self, tokens, idx):
+        if len(tokens) <= idx:
+            return idx
+        name = tokens[idx].type
+        value = tokens[idx].value
+        if name == 'HASH':
+            if self.id != None:
+                raise ParseError("Dupilicate id in simple selector")
+            self.id = value[1:]
+            return self._parse_simple_suffix(tokens, idx + 1)
+        elif name == 'DELIM' and value == '.':
+            if not len(tokens) > idx + 1 and tokens[idx + 1].name == 'IDENT':
+                raise ParseError("Missing identifier after '.'")
+            self.cls.append(tokens[idx + 1].value)
+            return self._parse_simple_suffix(tokens, idx + 2)
+        else:
+            # XXX: implement attributes and psuedo elements
+            raise ParseError("Expected identifier or class, found neither.")
 
-    def style_check(self):
-        for style in self.styles:
-            if not style.used:
-                logging.warn("Style '%s' not used." % style.name)
 
+    def child_match(self, name=None, id=None, cls=[]):
+        return False # TODO
 
-class Style(dict):
-    """A single style with multiple states, used internally"""
-    def __init__(self, name, states):
-        self.used = False
-        self.name = name
-        self.update(states)
-        self.deep = []
-        for part in name.split():
-            (tid, cls, oid) = (None, None, None)
-            if '.' in part:
-                (tid, part) = part.split('.')
-            if '#' in part:
-                (cls, oid) = part.split('#')
-            elif part:
-                cls = part
-            self.deep.append([tid, cls, oid])
-        self.shallow = self.deep[-1]
+    def descendant_match(self, name=None, id=None, cls=[]):
+        return False # TODO
 
-    def style_match(self, tid, oid, classes):
-        """Returns True if this style matches the given ids"""
-        (xtid, xcls, xoid) = self.shallow
-        if xtid not in (None, '') and tid != xtid:
+    def match(self, name=None, id=None, cls=[]):
+        # Make sure the element type matches.
+        if self.name != None and name != self.name:
             return False
-        if xoid not in (None, '') and oid != xoid:
+        # same for the id
+        if self.id != None and id != self.id:
             return False
-        if xcls not in (None, '') and xcls not in classes:
-            return False
+        # Make sure all of required the cls are there.
+        if type(cls) == type(''):
+            cls = [cls]
+        for cls in self.cls:
+            if not cls in cls:
+                return False
+        # Nothing missing.
         return True
 
-
-class Styles(list):
-    """Represent a chain of styles and allow access to getting properties"""
-    def get_properties(self, states, changed, otype=None):
-        """Get the loaded style properties, return a default value
-        if not found, or if no default set, property ignored."""
-        props = {}
-        if changed and changed in states:
-            states = [ changed ]
-        for state in states:
-            props.update(self.get_state(state))
-        return props.iteritems()
-
-    def get_state(self, state):
-        """Gets the combined states for this style collection"""
-        result = {}
-        for style in self:
-            result.update(style.get(state, {}))
-        return result
-
-    def apply_properties(self, states, changed, target):
-        """Apply properties from the css and catch problems"""
-        for name, value in self.get_properties(states, changed):
-             try:
-                 target(name, *self.get_value(name, value))
-             except TypeError, error:
-                 logging.error("Can't set '%s' to '%s': %s" % \
-                     (name, value, "(value not acceptable)"))
-             except KeyError, error:
-                 logging.error("Can't set '%s' to '%s': %s" % \
-                     (name, value, str(error)))
-
-    def get_value(self, name, value=None):
-        """Get value for property, or default value"""
-        value = GlobalStyles.get_value(name, value)
-        if not isinstance(value, tuple):
-            value = value,
-        return value
-
-GlobalStyles = StyleController()
