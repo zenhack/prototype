8d1199c0af6a4cee5cce1a58b2d8b38648602721
Ian Denhardt <ian@zenhack.net>
2014-06-29 19:15:42 -0400

    Moved requirements to requirements.txt.
    This lets pip do the right thing.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+
 setup(name='sterling',
       version='0.1',
       author='Ian Denhardt',
       author_email='ian@zenhack.net',
       url='https://github.com/sterling-ui/prototype',
       packages=find_packages(),
-      requires=['xssd'],
+      requires=[str(r.req) for r in install_reqs],
      )
