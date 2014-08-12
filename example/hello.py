1d1babc100f1a8411446669ac33489bb71486ed7
Ian Denhardt <ian@zenhack.net>
2014-08-11 21:06:13 -0400

    Move the test app to the examples folder.
    It has been renamed to hello.

--- example/hello.py
+++ example/hello.py
@@ -1,14 +1,14 @@
 from sterling.model import Model
 
 
-class Test(Model):
+class Hello(Model):
 
     def __init__(self):
-        super(Test, self).__init__()
+        super(Hello, self).__init__()
         self.first = 'hello'
         self.last = 'goodbye'
 
     def do_it(self, o):
         print("Done!")
 
-Test().run()
+Hello().run()
