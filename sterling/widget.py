6195e58b630f64a38134612c9b2b0925e44aa542
Ian Denhardt <ian@zenhack.net>
2014-08-20 08:14:03 -0400

    Start separating widgets and frames.
    The code is in a broken state right now; some parts have to be updated
    to make it work again.
    
    This is the beginnings of an effort to separate widgets from their
    frames. Frames are a structural specification, which does not change
    based on run-time events. Widgets, on the other hand, correspond to
    actual graphical components. They are generated from the frame/spec,
    which should not be polluted by state of a particular instance.

--- sterling/widget.py
+++ sterling/widget.py
@@ -22,32 +22,70 @@ EFL based widgets to display the kinds of things we want... more docs to follow.
 from sterling.frame import Frame
 from efl.elementary import window, box, button
 
+from abc import ABCMeta, abstractmethod
+
+
+class Widget(object):
+    """A GUI widget (in the traditional sense).
+
+    This class acts as a wrapper around the underlying toolkit's widget
+    objects. It may actually comprise multiple widgets; see `raw_contents()`.
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def raw(self):
+        """Return the underlying toolkit widget.
+
+        This is useful mainly for implementing other widgets, as it allows the
+        caller to e.g. add them to container widgets.
+
+        Access to the underlying toolkit object is useful for some backends,
+        but is not a substitute for the `add()` method. Toolkit-agnostic code
+        should not use this method.
+        """
+
+    @abstractmethod
+    def raw_contents(self):
+        """Return the underlying container widget.
+
+        The return value should be a container widget in the underlying
+        toolkit, capable of containing multiple child widgets. This may not be
+        the same as `raw()`. For example, if the result of `raw()` is unable
+        to contain more than one child, the implementation may add another
+        widget (which can contain more than one widget) as its child, and
+        return that from `raw_contents()`.
+
+        Access to the underlying toolkit object is useful for some backends,
+        but is not a substitute for the `add()` method. Toolkit-agnostic code
+        should not use this method.
+        """
+
+    @abstractmethod
+    def add(self, child):
+        """Add the child widget `child`."""
+
+
 class Window(Frame):
     """A widow widget based ontop of the windows and box EFL widgets"""
 
     def make_widget(self, data, parent=None):
-        if not hasattr(self, '_win'):
-            if not hasattr(self, 'horizontal'):
-                # XXX: The 'horizontal' boolean is a pretty lousy way to talk
-                # about this; what we'd really like is something like
-                # 'orientation', with an enum as a value. I don't see a reason
-                # to propogate the efl design wart up to our users. For now
-                # though, it's easy to just wrap the existing semantics.
-                self.horizontal = False
+        return self.widget_cls(data)
+
+    class widget_cls(Widget):
+
+        def __init__(self):
             self._win = window.StandardWindow('test', 'Test')
             self._box = box.Box(self._win)
-            self._box.horizontal_set(self.horizontal)
             self._win.resize_object_add(self._box)
             self._box.show()
             self._win.show()
 
-        return self._win
+        def raw_contents(self):
+            return self._box
 
-    def efl_container(self):
-        return self._box
-
-    def add(self, w):
-        self._box.pack_end(w)
+        def add(self, child):
+            self._box.pack_end(child.raw())
 
 
 class Button(Frame):
